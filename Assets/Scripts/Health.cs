15f6188925eb11f2b07d17bc65b962b89efbfe53
Wilson-WJK <wjk25a@acu.edu>
2025-11-19 14:53:57 -0600

    Hurt and Death animations, functional health bar

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,32 +1,34 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
-        if (currentHealth < 0)
+        if (currentHealth > 0)
         {
-            //player hurt
+            anim.SetTrigger("SGHurt");
         }
         else
         {
-            //player dead
+            if (!dead)
+            {
+                anim.SetTrigger("SGDeath");
+                GetComponent<SideScrollPlayer>().enabled = false;
+                dead = true;
+            }
         }
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-            TakeDamage(1);
     }
 }
