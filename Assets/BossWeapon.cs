64b6d0f0745a24713dc6bc37d2595cd6a97fd692
amt24a-wq <amt24a@acu.edu>
2025-12-08 16:15:22 -0600

    boss stuff

---
+++ Assets/BossWeapon.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BossWeapon : MonoBehaviour
+{
+
+    public int attackDamage = 1;
+
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
+
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+
+        if (colInfo != null)
+        {
+            //damage the player
+            PlayerController player = colInfo.GetComponent<PlayerController>();
+            if (player != null && player.IsParrying())
+            {
+                player.SpawnParrySparks();
+
+                return; // Do NOT damage the player
+            }
+            colInfo.GetComponent<Health>().TakeDamage(attackDamage);
+        }
+
+    }
+}
+
