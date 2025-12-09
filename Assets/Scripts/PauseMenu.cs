c68b17bc6e64ff7b7deb866c0b13a0ced0868e1e
TotesNotClarke <clarkevinson@gmail.com>
2025-12-09 01:48:24 -0600

    Charliefoxtrot
    I literally couldnt get anything to work im going to go spear fishing at the community pool or something I feel so useless and also there are bugs everywhere

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+     
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)) {
+                
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToStart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartingScreen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
